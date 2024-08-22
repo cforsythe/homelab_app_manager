7c80190a74a3a71f5770f9fd486977b180583ddc
cforsythe <cforsythe.cs@gmail.com>
2024-08-22 04:55:31 +0000

    Use http instead of https for img urls

--- config/app.js
+++ config/app.js
@@ -1,20 +1,20 @@
 module.exports = [
     // Internal Apps
     // Great for finding icons: https://iconduck.com/
-    { name: 'Proxmox', url: 'https://192.168.1.61:8006', image: "https://static-00.iconduck.com/assets.00/proxmox-icon-512x512-wdyg8x2z.png", category: 'internal' },
+    { name: 'Proxmox', url: 'https://192.168.1.61:8006', image: "http://static-00.iconduck.com/assets.00/proxmox-icon-512x512-wdyg8x2z.png", category: 'internal' },
     { name: 'iDRAC', url: 'https://192.168.1.60', image: "https://pngimg.com/uploads/server/server_PNG39.png", category: 'internal' },
-    { name: 'Home Assistant', url: 'http://hosting.local:8123', image: "https://static-00.iconduck.com/assets.00/home-assistant-icon-512x512-5t6h1s28.png", category: 'internal' },
-    { name: 'OpenMediaVault', url: 'http://openmediavault.local', image: "https://static-00.iconduck.com/assets.00/openmediavault-icon-512x512-i9cpzias.png", category: 'internal' },
+    { name: 'Home Assistant', url: 'http://hosting.local:8123', image: "http://static-00.iconduck.com/assets.00/home-assistant-icon-512x512-5t6h1s28.png", category: 'internal' },
+    { name: 'OpenMediaVault', url: 'http://openmediavault.local', image: "http://static-00.iconduck.com/assets.00/openmediavault-icon-512x512-i9cpzias.png", category: 'internal' },
     { name: 'Prusa Octoprint', url: 'http://prusa.local', image: "https://octoprint.org/assets/img/logo.png", category: 'internal' },
     { name: 'Creality Octoprint', url: 'http://octopi.local', image: "https://octoprint.org/assets/img/logo.png", category: 'internal' },
-    { name: 'qBittorrent', url: 'http://hosting.local:8080/', image: "https://static-00.iconduck.com/assets.00/qbittorrent-icon-512x512-hg06g855.png", category: 'internal' },
-    { name: 'Sonarr', url: 'http://hosting.local:8989', image: "https://static-00.iconduck.com/assets.00/sonarr-icon-512x512-u69bke0s.png", category: 'internal' },
-    { name: 'Radarr', url: 'http://hosting.local:7878', image: "https://static-00.iconduck.com/assets.00/radarr-icon-462x512-bydv4e4f.png", category: 'internal' },
-    { name: 'Prowlarr', url: 'http://hosting.local:9696', image: "https://static-00.iconduck.com/assets.00/prowlarr-icon-512x512-v9ekdjxx.png", category: 'internal' },
+    { name: 'qBittorrent', url: 'http://hosting.local:8080/', image: "http://static-00.iconduck.com/assets.00/qbittorrent-icon-512x512-hg06g855.png", category: 'internal' },
+    { name: 'Sonarr', url: 'http://hosting.local:8989', image: "http://static-00.iconduck.com/assets.00/sonarr-icon-512x512-u69bke0s.png", category: 'internal' },
+    { name: 'Radarr', url: 'http://hosting.local:7878', image: "http://static-00.iconduck.com/assets.00/radarr-icon-462x512-bydv4e4f.png", category: 'internal' },
+    { name: 'Prowlarr', url: 'http://hosting.local:9696', image: "http://static-00.iconduck.com/assets.00/prowlarr-icon-512x512-v9ekdjxx.png", category: 'internal' },
     // Public Apps
     { name: 'BookStack', url: 'https://bookstack.cforsythe.me', image: "https://avatars.githubusercontent.com/u/20912696?s=200&v=4", category: 'public' },
-    { name: 'Overseerr', url: 'https://watch.cforsythe.me', image: "https://static-00.iconduck.com/assets.00/overseerr-icon-512x512-i9uxvsi3.png", category: 'public' },
-    { name: 'Plex', url: 'https://hosting.local:32400', image: "https://static-00.iconduck.com/assets.00/plex-new-icon-512x512-k93c4jua.png", category: 'public' },
+    { name: 'Overseerr', url: 'https://watch.cforsythe.me', image: "http://static-00.iconduck.com/assets.00/overseerr-icon-512x512-i9uxvsi3.png", category: 'public' },
+    { name: 'Plex', url: 'https://hosting.local:32400', image: "http://static-00.iconduck.com/assets.00/plex-new-icon-512x512-k93c4jua.png", category: 'public' },
     { name: 'AMP', url: 'https://minecraft.cforsythe.me', image: "https://avatars.githubusercontent.com/u/9978589?s=200&v=4", category: 'public' },
-    { name: 'Mealie', url: 'https://mealie.cforsythe.me', image: "https://static-00.iconduck.com/assets.00/mealie-icon-512x489-939jw8dj.png", category: 'public' },
+    { name: 'Mealie', url: 'https://mealie.cforsythe.me', image: "http://static-00.iconduck.com/assets.00/mealie-icon-512x489-939jw8dj.png", category: 'public' },
 ];
